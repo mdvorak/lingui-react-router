1103ca679b53b1d45f2f74be107e2e595065f74e
Michal Dvořák <484607+mdvorak@users.noreply.github.com>
2025-09-24 11:05:25 +0200

    Simplify build configuration by removing options

--- build.config.ts
+++ build.config.ts
@@ -1,13 +1,5 @@
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
-  entries: ['src/index'],
-  declaration: true,
-  clean: true,
   outDir: 'dist',
-  rollup: {
-    emitCJS: true,
-    cjsBridge: true,
-  },
-  externals: ['react', 'react-dom', 'react-router-dom', '@lingui/react']
 })
