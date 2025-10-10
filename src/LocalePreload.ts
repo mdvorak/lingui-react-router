8096cbb24788589cd19eae3fcb1c09869d74d426
Michal Dvorak <mikee2185@gmail.com>
2025-10-10 21:41:08 +0200

    refactor: add defaultLocale export

--- src/LocalePreload.ts
+++ src/LocalePreload.ts
@@ -1,12 +1,12 @@
 import React from "react"
 import { useLocation } from "react-router"
 import manifest from "virtual:lingui-router-manifest"
-import { parseUrlLocale } from "./runtime"
+import { defaultLocale, parseUrlLocale } from "./runtime"
 
 export function LocalePreload() {
   if ((import.meta as any).env.SSR) {
     const { pathname } = useLocation()
-    const { locale } = parseUrlLocale(pathname)
+    const locale = parseUrlLocale(pathname).locale ?? defaultLocale
 
     if (locale && locale in manifest) {
       const href = ((import.meta as any).env.BASE_URL ?? "") + manifest[locale].replace(/^\//, "")
