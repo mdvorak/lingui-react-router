d9a6fd9cff6cb782e2769f49310c3c7d0e581fcf
Michal Dvořák <mikee2185@gmail.com>
2025-11-05 15:11:27 +0100

    fix: remove I18App dependency on usePathLocale
    - This will be slightly faster, and allows further usePathLocale optimization

--- src/components/I18nApp.tsx
+++ src/components/I18nApp.tsx
@@ -1,8 +1,9 @@
 import { I18nProvider } from "@lingui/react"
-import React, { useEffect } from "react"
+import React, { useEffect, useMemo } from "react"
+import { useParams } from "react-router"
 import { $getI18nInstance } from "virtual:lingui-router-loader"
-import { usePathLocale } from "../i18n"
-import { loadLocaleCatalog } from "../runtime"
+import { findLocale } from "../i18n"
+import { config, loadLocaleCatalog } from "../runtime"
 
 /**
  * The I18nApp component provides internationalization context to the application.
@@ -43,8 +44,17 @@ import { loadLocaleCatalog } from "../runtime"
  * @param props.children - The child components to be rendered within the i18n context
  */
 export function I18nApp({ children }: Readonly<{ children: React.ReactNode }>) {
-  const { locale } = usePathLocale()
-  const i18n = $getI18nInstance(locale)
+  const localeParamName = config.localeParamName
+  const params = useParams()
+
+  const { i18n, locale } = useMemo(() => {
+    const localeParam = params[localeParamName]
+    const resolvedLocale = findLocale(localeParam).locale ?? config.defaultLocale
+    return {
+      locale: resolvedLocale,
+      i18n: $getI18nInstance(resolvedLocale),
+    }
+  }, [params[localeParamName]])
 
   useEffect(() => {
     // This is executed only client-side
