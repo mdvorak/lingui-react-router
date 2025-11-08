2ce7ffd9d653fdfcf157ce74a23731e0110333da
Michal Dvořák <mikee2185@gmail.com>
2025-11-08 19:37:03 +0100

    fix: update locale-module-parser tests to use async glob and bump @types/node version

--- test/fixtures/merged-catalogs.test.ts
+++ test/fixtures/merged-catalogs.test.ts
@@ -1,9 +1,10 @@
 import * as fs from "node:fs"
+import fg from "fast-glob"
 import { describe, expect, it } from "vitest"
 
-describe("locale-module-parser should merge catalog variables", () => {
+describe("locale-module-parser should merge catalog variables", async () => {
   // Starting at cwd
-  const catalogFiles = fs.globSync("build/client/assets/locale-*.js")
+  const catalogFiles = await fg("build/client/assets/locale-*.js")
   expect(catalogFiles).toHaveLength(5)
 
   it.each(catalogFiles)("should have only one JSON.parse() call in %s", catalogFile => {
