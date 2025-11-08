c987f554c6e914ba0a7ae09886a745c038ac739a
Michal Dvořák <mikee2185@gmail.com>
2025-11-08 19:40:53 +0100

    test: refactor merged-catalogs tests to use beforeAll and improve readability

--- test/fixtures/merged-catalogs.test.ts
+++ test/fixtures/merged-catalogs.test.ts
@@ -1,18 +1,19 @@
 import * as fs from "node:fs"
 import fg from "fast-glob"
-import { describe, expect, it } from "vitest"
+import { beforeAll, describe, expect, it } from "vitest"
 
 describe("locale-module-parser should merge catalog variables", () => {
   // Starting at cwd
-  let catalogFiles: string[] = [];
+  let catalogFiles: string[] = []
 
   beforeAll(async () => {
-    catalogFiles = await fg("build/client/assets/locale-*.js");
-  });
+    catalogFiles = await fg("build/client/assets/locale-*.js")
+  })
 
   it("should find 5 catalog files", () => {
-    expect(catalogFiles).toHaveLength(5);
-  });
+    expect(catalogFiles).toHaveLength(5)
+  })
+
   it.each(catalogFiles)("should have only one JSON.parse() call in %s", catalogFile => {
     const code = fs.readFileSync(catalogFile, "utf-8")
     expect(code.match(/JSON\.parse/g)).toHaveLength(1)
